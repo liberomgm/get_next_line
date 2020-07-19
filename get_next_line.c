0a52265bf55d42f0c06e66acad36e6a87bf09b6e
student <student@iMac-student.local>
2020-07-19 18:30:10 +0300

    valgrind

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,10 @@
 /*   By: loscar <loscar@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 09:17:48 by student           #+#    #+#             */
-/*   Updated: 2020/07/18 09:43:13 by loscar           ###   ########.fr       */
+/*   Updated: 2020/07/19 18:26:26 by loscar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <fcntl.h>
 #include "get_next_line.h"
 
 char *ft_strnew(size_t size)
@@ -44,7 +42,7 @@ char *check_remainder(char *remainder, char **line)
 		{
 			*p_n = '\0';
 			*line = ft_strdup(remainder);
-			remainder = ft_strdup(++p_n);
+			ft_strcpy(remainder, ++p_n);
 		}
 		else
 		{
@@ -65,7 +63,7 @@ int get_next_line(int fd, char **line)
 	char *p_n;
 	char *tmp;
 	
-	if (fd < 0 || line == NULL)
+	if (fd < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
 	p_n = check_remainder(remainder, line);
 	while (!p_n && (byte_was_read = read(fd, buf, BUFFER_SIZE)))
@@ -77,7 +75,7 @@ int get_next_line(int fd, char **line)
 			remainder = ft_strdup(++p_n);
 		}
 		tmp = *line;
-		*line = ft_strjoin(*line, (char*)buf);
+		*line = ft_strjoin(*line, buf);
 		free(tmp);
 	}
 	if (remainder == NULL)
@@ -85,8 +83,5 @@ int get_next_line(int fd, char **line)
 	else if (byte_was_read || ft_strlen(remainder) || ft_strlen(*line))
 		return (1);
 	else
-	{
-		free(remainder);
 		return (0);
-	}
 }
